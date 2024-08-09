1b1952cb5450ea2a1df42f9f7577bc57a2d567ef
Maxime Besse <bss@Maximes-MacBook-Pro.local>
2024-08-09 13:16:56 +0200

    update

--- tidal/BootTidal.hs
+++ tidal/BootTidal.hs
@@ -1,30 +1,27 @@
+:set -fno-warn-orphans
+:set -XMultiParamTypeClasses
 :set -XOverloadedStrings
 :set prompt ""
 
+-- Import all the boot functions and aliases.
+import           Sound.Tidal.Boot
+import           Data.Char          as Char
+import qualified Data.Map.Strict    as Map
 
-
-
-
-import qualified Control.Concurrent.MVar as MV
-
-import Sound.Tidal.Context
-
-import System.IO (hSetEncoding, stdout, utf8)
-
-hSetEncoding stdout utf8
+default (Pattern String, Integer, Double)
 
 :{
     mspTarget = Target {
         oName      = "msp",
         oAddress   = "127.0.0.1",
         oHandshake = False,
-        oPort      = 7000,
+        oPort      = 7400,
         oBusPort   = Nothing,
         oLatency   = 0.02,
         oSchedule  = Pre BundleStamp,
         oWindow    = Nothing
         }
-    mspFormat = OSC "/msp/play/" $ Named {requiredArgs = ["msp"]}
+    mspFormat = OSC "/tidal/event/" $ Named {requiredArgs = ["msp"]}
     msp = pS "msp"
     bus = pI "bus"
 
@@ -47,50 +44,45 @@ hSetEncoding stdout utf8
     -- oscmap = [(superdirtTarget, [superdirtShape])]
 :}
 
-tidal <- startStream (defaultConfig {cCtrlAddr = "0.0.0.0"}) oscmap
+-- Create a Tidal Stream with the default settings.
+-- Use 'mkTidalWith' to customize these settings.
+tidalInst <- mkTidalWith defaultConfig oscmap
+-- tidalInst <- mkTidal
 
--- tidal <- startTidal (superdirtTarget {oLatency = 0.1, oAddress = "127.0.0.1", oPort = 57120}) (defaultConfig {cFrameTimespan = 1/20})
--- total latency = oLatency + cFrameTimespan
+-- This orphan instance makes the boot aliases work!
+-- It has to go after you define 'tidalInst'.
+instance Tidally where tidal = tidalInst
 
+-- You can add your own aliases in this file. Here are some examples:
 :{
-let only = (hush >>)
-    p = streamReplace tidal
-    hush = streamHush tidal
-    panic = do hush
-               once $ sound "superpanic"
-    list = streamList tidal
-    mute = streamMute tidal
-    unmute = streamUnmute tidal
-    unmuteAll = streamUnmuteAll tidal
-    unsoloAll = streamUnsoloAll tidal
-    solo = streamSolo tidal
-    unsolo = streamUnsolo tidal
-    once = streamOnce tidal
-    first = streamFirst tidal
-    asap = once
-    nudgeAll = streamNudgeAll tidal
-    all = streamAll tidal
-    resetCycles = streamResetCycles tidal
-    setcps = asap . cps
-    getcps = streamGetCPS tidal
-    getnow = streamGetNow tidal
+let 
+    all                         = streamAll tidal
+-- Transitions --
+    xfade i                     = transition tidal True  (Sound.Tidal.Transition.xfadeIn 4) i
+    xfadeIn i t                 = transition tidal True  (Sound.Tidal.Transition.xfadeIn t) i
+    histpan i t                 = transition tidal True  (Sound.Tidal.Transition.histpan t) i
+    wait i t                    = transition tidal True  (Sound.Tidal.Transition.wait t) i
+    waitT i f t                 = transition tidal True  (Sound.Tidal.Transition.waitT f t) i
+    jump i                      = transition tidal True  (Sound.Tidal.Transition.jump) i
+    jumpIn i t                  = transition tidal True  (Sound.Tidal.Transition.jumpIn t) i
+    jumpIn' i t                 = transition tidal True  (Sound.Tidal.Transition.jumpIn' t) i
+    jumpMod i t                 = transition tidal True  (Sound.Tidal.Transition.jumpMod t) i
+    jumpMod' i t p              = transition tidal True  (Sound.Tidal.Transition.jumpMod' t p) i
+    mortal i lifespan release   = transition tidal True  (Sound.Tidal.Transition.mortal lifespan release) i
+    interpolate i               = transition tidal True  (Sound.Tidal.Transition.interpolate) i
+    interpolateIn i t           = transition tidal True  (Sound.Tidal.Transition.interpolateIn t) i
+    clutch i                    = transition tidal True  (Sound.Tidal.Transition.clutch) i
+    clutchIn i t                = transition tidal True  (Sound.Tidal.Transition.clutchIn t) i
+    anticipate i                = transition tidal True  (Sound.Tidal.Transition.anticipate) i
+    anticipateIn i t            = transition tidal True  (Sound.Tidal.Transition.anticipateIn t) i
+    forId i t                   = transition tidal False (Sound.Tidal.Transition.mortalOverlay t) i
 :}
 
-:{
-let setI = streamSetI tidal
-    setF = streamSetF tidal
-    setS = streamSetS tidal
-    setR = streamSetR tidal
-    setB = streamSetB tidal
-:}
+:script "/Users/bss/Dropbox/bsupercollider/tidal/functions-tidal2.tidal"
+:script "/Users/bss/Dropbox/bsupercollider/tidal/functions-midi.tidal"
+:script "/Users/bss/Dropbox/bsupercollider/tidal/functions-superdirt.tidal"
+:script "/Users/bss/Dropbox/bsupercollider/tidal/functions-msp.tidal"
 
+:set -fwarn-orphans
 :set prompt "tidal> "
 :set prompt-cont ""
-
-default (Pattern String, Integer, Double)
-
-:script tidal/functions-tidal.tidal
-:script tidal/functions-superdirt.tidal
-:script tidal/functions-msp.tidal
-:script tidal/func2.tidal
-:script tidal/functions-midi.tidal
